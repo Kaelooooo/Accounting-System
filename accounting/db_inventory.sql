b2e6dc386baf0a45f4ef3d8d9abea5beb8add0c5
Kaelooooo <agoncillo.santiago@gmail.com>
2023-07-06 17:06:07 +0800

    Update db_inventory.sql

--- accounting/db_inventory.sql
+++ accounting/db_inventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 30, 2023 at 09:51 AM
+-- Generation Time: Jul 05, 2023 at 04:16 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
